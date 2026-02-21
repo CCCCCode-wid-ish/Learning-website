f6cd0f046f49f53b84920c4a7638856fbd39907f
AishwaryaAmin <185823999+AishwaryaAmin@users.noreply.github.com>
2026-02-21 20:30:02 -0300

    Authentication

--- RBA/src/index.js
+++ RBA/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const dbConnect = require("./config/dbConnect.js");
 const authRoutes = require("./routes/authRoutes");
-
+const generaluserRoutes = require("./routes/generaluserRoutes")
 const app = express();
 
 // Middleware
@@ -11,7 +11,7 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 //Importing the genraluserRouter
-app.use("/")
+app.use("/api/generaluser", generaluserRoutes);
 
 // Start the server FIRST, then connect to DB
 const PORT = process.env.PORT || 3001;
